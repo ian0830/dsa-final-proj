da1bb8d73b837994608224218a98a9353691c5dd
Ian Wang <970379@gmail.com>
2021-05-29 00:50:12 +0800

    sample from dsa final project instructions

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#include "api.h"
+
+// The testdata only contains the first 100 mails (mail1 ~ mail100)
+// and 2000 queries for you to debug.
+
+int n_mails, n_queries;
+mail *mails;
+query *queries;
+
+int main(void) {
+    api.init(&n_mails, &n_queries, &mails, &queries);
+
+    for (int i = 0; i < n_queries; i++)
+        if (queries[i].type == expression_match)
+            api.answer(queries[i].id, NULL, 0);
+
+    return 0;
+}
