b70db59fe8e3fbe35ded25c6f8e450ceebaaf807
Ian Wang <970379@gmail.com>
2021-06-25 00:19:54 +0800

    tokenBst.h

--- tokenBst.h
+++ tokenBst.h
@@ -1,3 +1,7 @@
+#define contentSize 100000
+#define encodingSpace 62
+#define upperBound 2147483647
+
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,25 +12,72 @@
 typedef struct node {
     long long checksum;
     char* token;
+    struct node* left;
+    struct node* right;
 } Node;
 
-long long calculateChecksum(char* string) {
+unsigned long long calculateChecksum(char* string) {
+    int index = 0;
+    unsigned long long checksum = 0;
+    while (string[index] != '\0') {
+        checksum = (checksum * encodingSpace + (int)string[index]) % upperBound;
+        index++;
+    }
+    return checksum;
     // remember not to overflow
 }
 
 void insert(Node* BST, char* string) {
-    int checksum = calculateChecksum(string);
+    // 某子樹的左邊必小於等於某子樹根
+    // 某子樹的右邊必嚴格大於某子樹根
+    unsigned long long checksum = calculateChecksum(string);
+    Node* node = malloc(sizeof(Node));
+    Node* pointer = BST;
+    node->checksum = checksum;
+    node->token = string;
+    node->left = NULL;
+    node->right = NULL;
+    while (true) {
+        if (pointer->checksum >= checksum) {
+            if (pointer->left == NULL) {
+                pointer->left = node;
+                break;
+            } else
+                pointer = pointer->left;
+        } else {
+            if (pointer->right == NULL) {
+                pointer->right = node;
+                break;
+            } else
+                pointer = pointer->right;
+        }
+    }
     // insert into BST
 }
 
-bool isToken() {
+bool isValid(char c) {
     // determine whether A-Za-z0-9
+    return (c >= 48 && c <= 57) || (c >= 65 && c <= 90) ||
+           (c >= 97 && c <= 122);
 }
 
 Node** generateTokenBSTs(mail* mails, int length) {
     Node** tokenBSTs = malloc(sizeof(Node*) * length);
     for (int i = 0; i < length; i++) {
         Node* tokenBST = tokenBSTs[i];
+        mail m = mails[i];
+        char token[contentSize + 1];
+        int tokenIndex = 0, contentIndex = 0;
+        while (m.content[contentIndex] != '\0') {
+            if (isValid(m.content[contentIndex])) {
+                token[tokenIndex++] = m.content[contentIndex];
+            } else if (tokenIndex != 0) {
+                token[tokenIndex] = '\0';
+                insert(tokenBST, token);
+                tokenIndex = 0;
+            }
+            contentIndex++;
+        }
         // parse token from each mail
     }
     return tokenBSTs;
